78a854b0a689ae500a50810154af7e27f9cad4be
Artemio <Artemio@DESKTOP-92BTGL5>
2023-04-16 16:17:58 -0300

    Agregar archivos de proyecto.

---
+++ Models/Producto.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Rocosa.Models
+{
+    public class Producto
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="Nombre del Producto es requerido")]
+        public string NombreProducto { get; set; }
+
+        [Required(ErrorMessage ="Descripcion Corta es Requerida")]
+        public string DescripcionCorta { get; set; }
+
+        [Required(ErrorMessage ="Descripcion del Producto es requerida")]
+        public string DescripcionProducto { get; set; }
+
+        [Required(ErrorMessage ="El Precio del Producto es Requerido")]
+        [Range(1, double.MaxValue, ErrorMessage ="El Precio debe de ser Mayor a cero")]
+        public double Precio { get; set; }
+
+        
+        public string? ImagenUrl { get; set; }
+
+        // Foreign Key
+
+
+        public int CategoriaId { get; set; }
+
+        [ForeignKey("CategoriaId")]
+        public virtual Categoria? Categoria { get; set; }
+
+        public int TipoAplicacionId { get; set; }
+
+        [ForeignKey("TipoAplicacionId")]
+        public virtual TipoAplicacion? TipoAplicacion { get; set; }
+
+
+    }
+}
